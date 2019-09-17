9fae65d5ec0d788ed784845d521a18c01d4593f8
kyhei <phage4645@gmail.com>
2019-09-17 21:09:44 +0900

    開発モードが重たいためnestとnuxtでコンテナを別個に実行

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 docker run --rm -itd \
--p 3000:3000 \
+--name nest-app \
 -p 3001:3001 \
+-v $(pwd):/opt/workspace \
+-w /opt/workspace \
+socket-ito:latest bash
+
+docker run --rm -itd \
+--name nuxt-app \
+-p 8000:3000 \
 -v $(pwd):/opt/workspace \
 -w /opt/workspace \
 socket-ito:latest bash
